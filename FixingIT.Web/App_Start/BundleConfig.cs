bdd6a3af10741960092170973e1c73fd0cdb96cd
Stellan Lindell <stiltet@gmail.com>
2013-06-25 01:51:06 +0200

    Worked some with FrontPage, Index (Update), CSS and bundles

--- FixingIT.Web/App_Start/BundleConfig.cs
+++ FixingIT.Web/App_Start/BundleConfig.cs
@@ -8,22 +8,16 @@ namespace FixingIT.Web
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            //bundles.ResetAll();
-
+            bundles.ResetAll();
+            
             // SCRIPTS
 
-            bundles.Add(new ScriptBundle("~/bundles/scripts/jquery").Include(
-                        "~/Scripts/jquery-{version}.min.js")
-            );
+            bundles.Add(new ScriptBundle("~/bundles/scripts/jquery").Include("~/Scripts/jquery-{version}.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/scripts/jquery-ui").Include(
-                        "~/Scripts/jquery-ui-{version}.min.js")
-            );
+            bundles.Add(new ScriptBundle("~/bundles/scripts/jquery-ui").Include("~/Scripts/jquery-ui-{version}.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/scripts/jquery-val").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*")
-            );
+            bundles.Add(new ScriptBundle("~/bundles/scripts/jquery-val").Include("~/Scripts/jquery.unobtrusive*",
+                        "~/Scripts/jquery.validate*"));
 
             //bundles.Add(new ScriptBundle("~/bundles/scripts/bootstrap").Include(
             //            "~/Scripts/bootstrap.min.js"
@@ -31,17 +25,11 @@ namespace FixingIT.Web
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/scripts/modernizr").Include(
-                        "~/Scripts/modernizr-*"
-            ));
+            bundles.Add(new ScriptBundle("~/bundles/scripts/modernizr").Include("~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/scripts/knockout").Include(
-                        "~/Scripts/knockout-{version}.js"
-            ));
+            bundles.Add(new ScriptBundle("~/bundles/scripts/knockout").Include("~/Scripts/knockout-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/scripts/own-scripts").Include(
-                        "~/Scripts/OwnScripts/*.js"
-            ));
+            bundles.Add(new ScriptBundle("~/bundles/scripts/own-scripts").Include("~/Scripts/OwnScripts/*.js"));
 
             // STYLES
 
@@ -50,29 +38,9 @@ namespace FixingIT.Web
             //            "~/Content/bootstrap-responsive.min.css"
             //            ));
 
-            bundles.Add(new StyleBundle("~/bundles/styles/jquery-ui").Include(
-                        "~/Content/themes/base/minified/jquery.ui.min.css",
-                        "~/Content/themes/base/minified/jquery.ui.accordion.min.css",
-                        "~/Content/themes/base/minified/jquery.ui.all.min.css",
-                        "~/Content/themes/base/minified/jquery.ui.autocomplete.min.css",
-                        "~/Content/themes/base/minified/jquery.ui.base.min.css",
-                        "~/Content/themes/base/minified/jquery.ui.button.min.css",
-                        "~/Content/themes/base/minified/jquery.ui.core.min.css",
-                        "~/Content/themes/base/minified/jquery.ui.datepicker.min.css",
-                        "~/Content/themes/base/minified/jquery.ui.dialog.min.css",
-                        "~/Content/themes/base/minified/jquery.ui.progressbar.min.css",
-                        "~/Content/themes/base/minified/jquery.ui.resizable.min.css",
-                        "~/Content/themes/base/minified/jquery.ui.selectable.min.css",
-                        "~/Content/themes/base/minified/jquery.ui.slider.min.css",
-                        "~/Content/themes/base/minified/jquery.ui.spinner.min.css",
-                        "~/Content/themes/base/minified/jquery.ui.tabs.min.css",
-                        "~/Content/themes/base/minified/jquery.ui.theme.min.css",
-                        "~/Content/themes/base/minified/jquery.ui.tooltip.min.css"
-            ));
+            bundles.Add(new StyleBundle("~/bundles/styles/jquery-ui-min").Include("~/Content/themes/base/minified/jquery.ui*"));
 
-            bundles.Add(new StyleBundle("~/bundles/styles/own-style").Include(
-                        "~/Content/Site.css"
-            ));
+            bundles.Add(new StyleBundle("~/bundles/styles/own-style").Include("~/Content/Site.css"));
         }
     }
 }
